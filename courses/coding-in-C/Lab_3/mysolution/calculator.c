9cb084f5cbbcc2f8c345b9e06ca32e5cec306c59
estherbirngruber <estherbirngruber@gmail.com>
2026-02-04 10:06:21 +0100

    calculator task

---
+++ courses/coding-in-C/Lab_3/mysolution/calculator.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main(){
+    float first_number;
+    float second_number;
+    int operation;
+    float solution; 
+
+    printf ("type your first number: ");
+    scanf("%f", &first_number);
+    getchar();
+
+    printf ("\ntype in your second number: ");
+    scanf("%f", &second_number);
+    getchar();
+
+    printf("\npossible arithmetic operations:\n");
+    printf("1: Addition\n");
+    printf("2: Subtraction\n");
+    printf("3: Multiplication\n");
+    printf("4: Division\n");
+    printf("type the number of the operation you want ");
+    scanf("%d", &operation);
+    getchar();
+
+    switch (operation) {
+        case 1:
+        solution = first_number + second_number;
+        printf("%.2f + %.2f = %.2f", first_number, second_number, solution);
+        break;
+        case 2:
+        solution = first_number - second_number;
+        printf("%.2f - %.2f = %.2f", first_number, second_number, solution);
+        break;
+        case 3:
+        solution = first_number * second_number;
+        printf("%.2f * %.2f = %.2f", first_number, second_number, solution);
+        break;
+        case 4:
+        solution = first_number / second_number;
+        if (second_number == 0) {
+            printf("division by zero not possible");
+        }
+        else {
+            printf("%.2f / %.2f = %.2f", first_number, second_number, solution);
+        }
+        break;
+        default:
+            printf("\nWrong Operator");
+        break;
+    }
+    getchar();
+
+    return 0;
+}
