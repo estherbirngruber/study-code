94bb38370fa2a5b1e6a57dbe392c93a5d3b324a6
estherbirngruber <estherbirngruber@gmail.com>
2026-02-13 12:00:14 +0100

    start section 2

---
+++ courses/coding-in-C/Lab_5/mysolution/Section2.ParticleSimulation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 10;
+    int *p = &x;
+    int **pp = &p;
+
+    // TODO: complete the output statements
+    printf("x = %d\n", x);
+    printf("x via p = %d\n", *p);
+    printf("x via pp = %d\n", *pp);
+    **pp = 42;
+    printf("x = %d\n", x);
+    getchar ();
+
+    return 0;
+}
+
+
+
+//4.    x ist ein integer
+//      p ist die Adresse von x
+//      *p ist der Pointer auf x
+//      pp ist die Adresse von Pointer p
+//      *pp ist der Pointer auf den Pointer p
