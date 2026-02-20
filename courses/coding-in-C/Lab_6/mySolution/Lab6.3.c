0905efb110b9302e9e5f3c4eafce226d014d54f6
estherbirngruber <estherbirngruber@gmail.com>
2026-02-20 14:36:12 +0100

    LAB 6 Section 3 not finished :( and moved sensor text files

--- courses/coding-in-C/Lab_6/mySolution/Lab6.3.c
+++ courses/coding-in-C/Lab_6/mySolution/Lab6.3.c
@@ -10,10 +10,27 @@ struct SensorData {
 struct Sensor {
     int id;
     double threshold;
-    SensorData *data;
-    int *object_detection;
+    struct SensorData data [3000];
+    int object_detection [3000];
 };
 
-int main () {
+void read_file (FILE *file, struct Sensor *sensor){
+    int i;
+    for (i=0; i<3000; i++){
+        fscanf (file, "%f %lf", &sensor->data[i].time, &sensor->data[i].probability);
+    }
+}
 
+int main () {
+    FILE *fileSensor1 = fopen ("sensor1.txt", "r");
+    FILE *fileSensor2 = fopen ("sensor2.txt", "r");
+
+    struct Sensor sensor1 = {1, 0.8};
+    struct Sensor sensor2 = {2, 0.7};
+
+    read_file(fileSensor1, &sensor1);
+    read_file(fileSensor2, &sensor2);
+
+    fclose(fileSensor1);
+    fclose(fileSensor2);
 }
