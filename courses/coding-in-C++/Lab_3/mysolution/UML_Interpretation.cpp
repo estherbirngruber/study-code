40242af3c44dd50a1bac23ed5766e1ca67852d71
estherbirngruber <estherbirngruber@gmail.com>
2026-04-27 15:24:58 +0200

    UML interpretation

---
+++ courses/coding-in-C++/Lab_3/mysolution/UML_Interpretation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+class User {
+    protected:
+        std::string name;
+        int id;
+    public:
+        User(std::string name, int id){
+            this->name = name;
+            this->id = id;
+        }
+        void printInfo(){
+            std::cout << "Name: " << name << ", ID: " << id << std::endl;
+        }
+
+};
+
+int main () {
+
+}
