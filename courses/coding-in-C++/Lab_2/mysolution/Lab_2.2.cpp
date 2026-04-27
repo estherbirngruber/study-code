bcbec96cad9bab7467e2ca6ae8d3ab426160e990
estherbirngruber <estherbirngruber@gmail.com>
2026-04-27 12:12:43 +0200

    lab 2.2 finished

--- courses/coding-in-C++/Lab_2/mysolution/Lab_2.2.cpp
+++ courses/coding-in-C++/Lab_2/mysolution/Lab_2.2.cpp
@@ -5,22 +5,31 @@ class Note {
     private:
         std::string* text;
     public:
-        Note(std::string text) {
-            while (true) {
-                std::cout << ("Type in your message: ");
-                if (std::getline(std::cin, text)){
-                break;
-                }
-            }
+        Note(std::string text_input) {
+          text = new std::string(text_input);
         }
         ~Note() {
             delete text;
+            text = nullptr;
+            std::cout << ("Memory released") << std::endl;
+        }
+        Note (const Note& other){
+            text = new std::string (*other.text);
         }
         void display(){
-            std::cout << ("");
+            std::cout << *text << std::endl;
         }
 };
 
 int main () {
 
+    Note object1 ("Hallo");
+    Note object2 (object1);
+    object1.display();
+    object2.display();
+    return 0;
 }
+
+//the copy constructor copies the adress automatically so thats why we can initialize the second object with the first. both object.text shows to the same adress.
+//the destructor is automatically called at the end of the scope so here end of main.
+//however, this leads to undefined behaviour if both objects are destructed, it leads to trying to free the memory of the objects two times, as the second object is a copy of the first.
