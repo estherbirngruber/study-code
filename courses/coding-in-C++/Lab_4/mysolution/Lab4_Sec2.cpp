462b9d6c75b0fd889c1b1650e74e65bcfc817a76
estherbirngruber <estherbirngruber@gmail.com>
2026-05-07 15:22:15 +0200

    Lab 4 psychose

---
+++ courses/coding-in-C++/Lab_4/mysolution/Lab4_Sec2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class shape {
+    private:
+    float defaultValue = 0.0;
+
+    public:
+    virtual double area() const {
+        return 0.0;
+    }
+};
+
+class Circle : public shape {
+    private:
+    float radius;
+    
+    public:
+    double area {
+
+    }
+};
+
+class Rectangle : public shape {
+    private:
+    float length;
+    float height;
+
+    public:
+    double area {
+        length
+    }
+}
+
+int main (){
+
+}
+
+
+WHAT THE F
